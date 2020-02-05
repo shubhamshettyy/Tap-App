f0a847001421162765d487a75610109e8088b734
Ritik Singh <ritik.singh@spit.ac.in>
2020-02-05 11:52:30 +0530

    added register

--- tsechack/settings.py
+++ tsechack/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'adminusers.apps.AdminusersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- tsechack/urls.py
+++ tsechack/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from adminusers import views as adminusersviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('tapapp/',adminusersviews.register,name="register")
 ]
