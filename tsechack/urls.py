e2d5a976924d2a0d9220f2df05c23d9de39520eb
shubhamshettyy <shubham.shetty@spit.ac.in>
2020-02-05 11:59:47 +0530

    commit

--- tsechack/urls.py
+++ tsechack/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from adminusers import views as adminusers_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/', adminusers_views.register, name="register"),
 ]
